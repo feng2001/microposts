04a24128d1a5f4432a64acebdeb0a07174f7c185
Atsushi Koike <koike@shiga777.com>
2016-09-10 20:46:55 +0000

    フォロー機能実装

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,7 @@ module Microposts
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
+
     config.active_record.raise_in_transactional_callbacks = true
     config.generators do |g|
       g.stylesheets false
@@ -28,5 +29,6 @@ module Microposts
       g.helper false
       g.test_framework false
     end
+    config.action_view.embed_authenticity_token_in_remote_forms = true
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,5 @@ Rails.application.routes.draw do
   #put   'edit' , to: 'users#update'
   resources :users
   resources :microposts
+  resources :relationships, only: [:create, :destroy]
 end
